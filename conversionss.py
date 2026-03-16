c12a0f0f5c017111e68b47f5db5ea3502967b10d
keondreblankson <43209933+keondreblankson@users.noreply.github.com>
2026-03-16 18:42:03 -0400

    Add temperature conversion functions in conversionss.py

---
+++ conversionss.py
@@ -0,0 +1,22 @@
+def convertCelsiusToKelvin(celsius):
+    return celsius + 273.15
+
+
+def convertCelsiusToFahrenheit(celsius):
+    return (celsius * 9/5) + 32
+
+
+def convertFahrenheitToCelsius(fahrenheit):
+    return (fahrenheit - 32) * 5/9
+
+
+def convertFahrenheitToKelvin(fahrenheit):
+    return (fahrenheit - 32) * 5/9 + 273.15
+
+
+def convertKelvinToCelsius(kelvin):
+    return kelvin - 273.15
+
+
+def convertKelvinToFahrenheit(kelvin):
+    return (kelvin - 273.15) * 9/5 + 32
